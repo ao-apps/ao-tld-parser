5547da51de65e758ab520b5495621748a628beb5
AO Industries, Inc <support@aoindustries.com>
2021-02-14 07:31:41 -0600

    Released ao-encoding-3.3.0

--- src/main/java/com/aoindustries/tld/parser/HtmlSnippet.java
+++ src/main/java/com/aoindustries/tld/parser/HtmlSnippet.java
@@ -1,6 +1,6 @@
 /*
  * ao-tld-parser - Parses JSP tag library *.tld files.
- * Copyright (C) 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2017, 2019, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,7 +22,7 @@
  */
 package com.aoindustries.tld.parser;
 
-import com.aoindustries.encoding.Coercion;
+import com.aoindustries.lang.Coercion;
 import java.io.StringReader;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
