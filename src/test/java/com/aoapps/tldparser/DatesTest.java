c05e9479206f3137226fc5fe2e91ef1092229ed1
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:27 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/test/java/com/aoapps/tldparser/DatesTest.java
+++ src/test/java/com/aoapps/tldparser/DatesTest.java
@@ -1,6 +1,6 @@
 /*
  * ao-tld-parser - Parses JSP tag library *.tld files.
- * Copyright (C) 2019, 2021  AO Industries, Inc.
+ * Copyright (C) 2019, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-tld-parser.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.tldparser;
 
 import java.util.regex.Matcher;
