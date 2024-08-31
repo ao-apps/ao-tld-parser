4fd487cc507d9fc3c672796c7b7aa0ae392de78a
AO Industries, Inc <support@aoindustries.com>
2024-08-31 16:50:23 -0500

    checkstyle-10.18.1 compatibility

--- src/main/java/com/aoapps/tldparser/Dates.java
+++ src/main/java/com/aoapps/tldparser/Dates.java
@@ -1,6 +1,6 @@
 /*
  * ao-tld-parser - Parses JSP tag library *.tld files.
- * Copyright (C) 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -129,7 +129,7 @@ public class Dates {
   }
 
   /**
-   * <a href="https://schema.org/dateCreated">https://schema.org/dateCreated</a>
+   * <a href="https://schema.org/dateCreated">https://schema.org/dateCreated</a>.
    *
    * @see  #getPublished()  When created and published are the same date, prefer
    *                            published because it seems to have more use overall than created.
@@ -140,7 +140,7 @@ public class Dates {
   }
 
   /**
-   * <a href="https://schema.org/datePublished">https://schema.org/datePublished</a>
+   * <a href="https://schema.org/datePublished">https://schema.org/datePublished</a>.
    *
    * @see  #getCreated()  When created and published are the same date, prefer
    *                          published because it seems to have more use overall than created.
@@ -151,7 +151,7 @@ public class Dates {
   }
 
   /**
-   * <a href="https://schema.org/dateModified">https://schema.org/dateModified</a>
+   * <a href="https://schema.org/dateModified">https://schema.org/dateModified</a>.
    */
   // Matches com.semanticcms.core.model.Page.getDateModified()
   public DateTime getModified() {
